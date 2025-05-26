a1bef914433cd28c1062565b02a103bfbfcfb2f6
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-26 21:54:48 +0100

    Created box plot of the data. Added trendlines and color to the scatter graph.

--- core/views.py
+++ core/views.py
@@ -8,8 +8,10 @@ from core.models import PersonSalary
 
 def plot(request):
     person_salaries = PersonSalary.objects.all()
+    # person_salaries = PersonSalary.objects.filter(education__in=['1. < HS Grad', '5. Advanced Degree'])
     ages = person_salaries.values_list('age', flat=True)
     salaries = person_salaries.values_list('salary', flat=True)
+    color = person_salaries.values_list('education', flat=True)
 
     fig = px.scatter(
         x=ages,
@@ -18,9 +20,22 @@ def plot(request):
         labels={"x": "Age", "y": "Salary"},
         height=800,
         trendline="ols",
+        color=color,
+    )
+
+    fig1 = px.box(
+        x=ages,
+        y=salaries,
+        title="Salary by age",
+        labels={"x": "Age", "y": "Salary"},
+        height=800,
     )
 
     html = fig.to_html()
+    html_box = fig1.to_html()
 
-    context = {'chart': html}
+    context = {
+        'chart': html,
+        'chart_box': html_box,
+    }
     return render(request, 'scatter.html', context)
